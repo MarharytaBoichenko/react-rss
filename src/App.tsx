976a9f480df3242be32ed2698fa69c548ecf4103
MarharytaBoichenko <boichenkomv@gmail.com>
2023-10-22 19:46:13 +0300

    init: add configs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    // query: '',
+  };
+
+  render() {
+    return <div className="container"></div>;
+  }
+}
+
+export default App;
